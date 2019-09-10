4ee7a994e52b13643f43e9814f28a8b605169bab
Khanson3 <khanson3@binghamton.edu>
2019-09-09 23:35:15 -0400

    Created design for title/data input page

--- app/src/main/java/edu/binghamton/khanson3/statplotter/MainActivity.java
+++ app/src/main/java/edu/binghamton/khanson3/statplotter/MainActivity.java
@@ -1,7 +1,14 @@
 package edu.binghamton.khanson3.statplotter;
 
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +16,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Spinner plotTypeSpinner = findViewById(R.id.plotTypeSpinner);
+        ArrayAdapter<CharSequence> plotTypes = ArrayAdapter.createFromResource(this, R.array.plot_types, android.R.layout.simple_spinner_item);
+        plotTypes.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        plotTypeSpinner.setAdapter(plotTypes);
+
+        plotTypeSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                ((TextView) adapterView.getChildAt(0)).setTextSize(15f);
+                //((TextView) adapterView.getChildAt(0)).setTypeface(Typeface.DEFAULT_BOLD);
+                ((TextView) adapterView.getChildAt(0)).setGravity(Gravity.CENTER);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
     }
 }
